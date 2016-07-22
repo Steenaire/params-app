86a4a741e6d670a5390eb5783595c33284b003b6
Steen Clarke <cmclarkey@gmail.com>
2016-07-21 21:31:24 -0700

    Add guessing game

--- app/controllers/params_examples_controller.rb
+++ app/controllers/params_examples_controller.rb
@@ -1,7 +1,21 @@
 class ParamsExamplesController < ApplicationController
   def query_parameters
     puts "params taken and outputted below"
-    @message = params[:message].reverse
-    @stuff = params[:coolness]
+    @name = params[:name].upcase
+    @message = ""
+    if @name[0] == "A"
+      @message = "Hey, your name starts with the first letter of the alphabet!"
+    end
+    @number = 36
+    @guess = params[:guess].to_i
+    @win_note = ""
+    if @number == @guess
+      @win_note = "You guessed it!"
+    elsif @number > @guess
+      @win_note = "Higher!"
+    elsif @number < @guess
+      @win_note = "Lower!"
+    end
+      
   end
 end
