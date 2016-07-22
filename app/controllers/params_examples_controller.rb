128e3ee68be5bca35d8086d85ab4083fc508dfd5
Steen Clarke <cmclarkey@gmail.com>
2016-07-22 16:05:10 -0700

    Refine guessing game to include random numbers

--- app/controllers/params_examples_controller.rb
+++ app/controllers/params_examples_controller.rb
@@ -6,10 +6,17 @@ class ParamsExamplesController < ApplicationController
     if @name[0] == "A"
       @message = "Hey, your name starts with the first letter of the alphabet!"
     end
-    @number = 36
+
+    file_number = File.open("public/number_game.txt").first.strip.to_i
+    @number = file_number    
+
     @guess = params[:guess].to_i
     @win_note = ""
+
     if @number == @guess
+      file = File.open("public/number_game.txt", 'w')
+      file.write("#{rand(100)}")
+      file.close
       @win_note = "You guessed it!"
     elsif @number > @guess
       @win_note = "Higher!"
